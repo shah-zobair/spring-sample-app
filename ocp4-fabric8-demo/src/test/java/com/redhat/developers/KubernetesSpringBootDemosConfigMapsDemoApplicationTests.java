8ca045d7d08325e60be7eb35da77ac7424c6d691
root <root@upi-0.ocp48b.lab.upshift.rdu2.redhat.com>
2021-10-23 18:44:08 -0400

    Added demo code for ocp4

---
+++ ocp4-fabric8-demo/src/test/java/com/redhat/developers/KubernetesSpringBootDemosConfigMapsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.developers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesSpringBootDemosConfigMapsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
